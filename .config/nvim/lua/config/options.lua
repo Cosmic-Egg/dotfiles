1718023920c8bc06a4cc2da3094adbe549dbb472
Cosmic-Egg <ekemeny18@gmail.com>
2024-07-14 13:54:23 -0600

    updated nvim

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- line wrapping
+vim.opt.wrap = false -- disable line wrapping
+
+-- split windows
+vim.opt.splitright = true -- split vertical window to the right
+vim.opt.splitbelow = true -- split horizontal window to the bottom
+
+-- tab settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
